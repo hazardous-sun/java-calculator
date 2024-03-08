8842002c55f03dc2aa65fa5a9328557917d09d17
solaire-notebook <contact@unharmfulhazard.com>
2024-03-08 15:40:32 -0300

    evalResult() was starting a process that was not being destroyed

--- src/python/JavaPythonCommunication.java
+++ src/python/JavaPythonCommunication.java
@@ -6,15 +6,20 @@ import java.io.IOException;
 
 public class JavaPythonCommunication {
     private static void evalResult(String input) {
+        Process process = null;
         try {
             ProcessBuilder processBuilder = new ProcessBuilder("python", "src/python/evaluate_result.py", input);
             processBuilder.redirectErrorStream(true);
-            Process process = processBuilder.start();
+            process = processBuilder.start();
             FileHandler.logResult(null, "Passed values to evaluate_result.py");
             process.waitFor();
             FileHandler.logResult(null, "evaluate_result.py ran without errors");
         } catch (IOException | InterruptedException e) {
             FileHandler.logResult(e, e.getMessage());
+        } finally {
+            if (process != null) {
+                process.destroy();
+            }
         }
     }
     public static String getResult(String input) {
