ac8c03caac265f403922ef3467dff460e7e61890
solaire-notebook <contact@unharmfulhazard.com>
2024-03-08 15:59:23 -0300

    Removed unused dependency

--- src/ui/Calculator.java
+++ src/ui/Calculator.java
@@ -4,8 +4,6 @@ import handlers.FileHandler;
 import python.JavaPythonCommunication;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Calculator extends JFrame {
     private JTextArea taScreen;
